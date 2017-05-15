233c4302ee33644364195eaa650111683f5e1d32
chenli <chenli@chens-air>
2017-05-15 09:28:38 +0800

    refactor

--- SpringBoot_Test/src/main/java/evolution/configuration/AnyBean.java
+++ SpringBoot_Test/src/main/java/evolution/configuration/AnyBean.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component// Don't forget to tag this class as the component so that you can @Autowired this pojo.
 @PropertySource("classpath:evolution.properties")// The default property source is application.properties, but you can specify your customized property source.
-@ConfigurationProperties("evolution")// This approach is recommended because it directly inject the properties in the properties file into the java pojo.
+@ConfigurationProperties("evolution")// This approach is recommended because it directly inject the properties in the properties file into the java pojo. evolution in this case is the prefix.
 public class AnyBean {
 	private String name;
 
