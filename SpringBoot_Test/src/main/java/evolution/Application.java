c3880594fc8366fc210d853d2d55322b10169140
chenli <chenli@chens-air>
2017-05-15 09:27:42 +0800

    new demos

--- SpringBoot_Test/src/main/java/evolution/Application.java
+++ SpringBoot_Test/src/main/java/evolution/Application.java
@@ -1,18 +1,23 @@
 package evolution;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import evolution.configuration.AnyBean;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+	@Autowired
+	private AnyBean anyBean;
 	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args).close();// Close the application automatically so that you won't need to shut down the project manually.
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
-
+		System.out.println(this.anyBean);
 	}
 }
---
+++ SpringBoot_Test/src/main/java/evolution/configuration/AnyBean.java
@@ -0,0 +1,25 @@
+package evolution.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component// Don't forget to tag this class as the component so that you can @Autowired this pojo.
+@PropertySource("classpath:evolution.properties")// The default property source is application.properties, but you can specify your customized property source.
+@ConfigurationProperties("evolution")// This approach is recommended because it directly inject the properties in the properties file into the java pojo.
+public class AnyBean {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "AnyBean [name=" + name + "]";
+	}
+}
